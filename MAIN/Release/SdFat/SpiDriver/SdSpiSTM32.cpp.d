b7aa1f6b0343cffebdd5854541539c33a692e0f8
Porokhnya Dmitry <spywarrior@gmail.com>
2018-08-22 21:33:09 +0300

    САША, НЕ НАДО ПАПКУ RELEASE В ГИТ!!!

--- MAIN/Release/SdFat/SpiDriver/SdSpiSTM32.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\MAIN\arduino_due_x_dbg\Release\SdFat\SpiDriver\SdSpiSTM32.cpp.o: \
- D:\1\ GIT_Dima_UROV\MAIN\libraries\SdFat\src\SpiDriver\SdSpiSTM32.cpp
