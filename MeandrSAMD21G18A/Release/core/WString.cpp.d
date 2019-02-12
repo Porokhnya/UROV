71d959f0bd9c708892408c2bb74c8af42ef9f1cf
promavto <promavto@ntmp.ru>
2019-02-12 11:10:49 +0300

    Фиксировать

---
+++ MeandrSAMD21G18A/Release/core/WString.cpp.d
@@ -0,0 +1,6 @@
+C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\MeandrSAMD21G18A\arduino_zero_native\Release\core\WString.cpp.o: \
+ C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\samd\1.6.19\cores\arduino\WString.cpp \
+ C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\samd\1.6.19\cores\arduino\WString.h \
+ C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\samd\1.6.19\cores\arduino/avr/pgmspace.h \
+ C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\samd\1.6.19\cores\arduino\itoa.h \
+ C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\samd\1.6.19\cores\arduino\avr/dtostrf.h
---
+++ MeandrSAMD21G18A/Release/core/pulse_asm.S.d
@@ -0,0 +1,2 @@
+C:\Users\moseichuk\AppData\Local\Temp\VMBuilds\MeandrSAMD21G18A\arduino_zero_native\Release\core\pulse_asm.S.o: \
+ C:\Users\moseichuk\AppData\Local\arduino15\packages\arduino\hardware\samd\1.6.19\cores\arduino\pulse_asm.S
