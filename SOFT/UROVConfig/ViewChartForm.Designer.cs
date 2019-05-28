ab87e74fef0b1cfc5c4eec189ddaa0b81dd8c288
Porokhnya Dmitry <spywarrior@gmail.com>
2019-05-28 17:19:23 +0300

    export

--- SOFT/UROVConfig/ViewChartForm.Designer.cs
+++ SOFT/UROVConfig/ViewChartForm.Designer.cs
@@ -28,16 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblCaption = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,7 +53,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(670, 40);
+            this.panel1.Size = new System.Drawing.Size(870, 40);
             this.panel1.TabIndex = 0;
             // 
             // lblCaption
@@ -68,48 +72,76 @@
             // panel2
             // 
             this.panel2.Controls.Add(this.chart);
+            this.panel2.Controls.Add(this.panel3);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 40);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(670, 451);
+            this.panel2.Size = new System.Drawing.Size(870, 548);
             this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnExport);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 484);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(870, 64);
+            this.panel3.TabIndex = 2;
             // 
             // chart
             // 
-            chartArea1.Name = "ethalonChartArea";
-            this.chart.ChartAreas.Add(chartArea1);
+            chartArea4.Name = "ethalonChartArea";
+            this.chart.ChartAreas.Add(chartArea4);
             this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
-            legend1.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Row;
-            legend1.Name = "Legend1";
-            this.chart.Legends.Add(legend1);
+            legend4.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend4.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Row;
+            legend4.Name = "Legend1";
+            this.chart.Legends.Add(legend4);
             this.chart.Location = new System.Drawing.Point(0, 0);
             this.chart.Name = "chart";
-            series1.BorderWidth = 6;
-            series1.ChartArea = "ethalonChartArea";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series1.Color = System.Drawing.Color.LightGreen;
-            series1.Legend = "Legend1";
-            series1.LegendText = "Эталон";
-            series1.Name = "serie1";
-            series2.BorderWidth = 2;
-            series2.ChartArea = "ethalonChartArea";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series2.Color = System.Drawing.Color.SteelBlue;
-            series2.Legend = "Legend1";
-            series2.LegendText = "Прерывание";
-            series2.Name = "serie2";
-            this.chart.Series.Add(series1);
-            this.chart.Series.Add(series2);
-            this.chart.Size = new System.Drawing.Size(670, 451);
-            this.chart.TabIndex = 1;
+            series7.BorderWidth = 6;
+            series7.ChartArea = "ethalonChartArea";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Color = System.Drawing.Color.LightGreen;
+            series7.Legend = "Legend1";
+            series7.LegendText = "Эталон";
+            series7.Name = "serie1";
+            series8.BorderWidth = 2;
+            series8.ChartArea = "ethalonChartArea";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Color = System.Drawing.Color.SteelBlue;
+            series8.Legend = "Legend1";
+            series8.LegendText = "Прерывание";
+            series8.Name = "serie2";
+            this.chart.Series.Add(series7);
+            this.chart.Series.Add(series8);
+            this.chart.Size = new System.Drawing.Size(870, 484);
+            this.chart.TabIndex = 3;
             this.chart.Text = "chart1";
+            // 
+            // btnExport
+            // 
+            this.btnExport.BackColor = System.Drawing.Color.LightGreen;
+            this.btnExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExport.ForeColor = System.Drawing.Color.Black;
+            this.btnExport.Location = new System.Drawing.Point(12, 12);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(167, 40);
+            this.btnExport.TabIndex = 9;
+            this.btnExport.Text = "Экспорт";
+            this.btnExport.UseVisualStyleBackColor = false;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Рисунок PNG|*.png|Рисунок JPEG|*.jpg";
+            this.saveFileDialog.RestoreDirectory = true;
             // 
             // ViewChartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(670, 491);
+            this.ClientSize = new System.Drawing.Size(870, 588);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "ViewChartForm";
@@ -120,6 +152,7 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -130,7 +163,10 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        public System.Windows.Forms.DataVisualization.Charting.Chart chart;
         public System.Windows.Forms.Label lblCaption;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
--- SOFT/UROVConfig/ViewChartForm.cs
+++ SOFT/UROVConfig/ViewChartForm.cs
@@ -15,5 +15,52 @@ namespace UROVConfig
         {
             InitializeComponent();
         }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                bool errors = false;
+                string fname = saveFileDialog.FileName;
+                switch(saveFileDialog.FilterIndex)
+                {
+                    case 1: // png
+                        {
+                            try
+                            {
+                                this.chart.SaveImage(fname, System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png);
+                            }
+                            catch
+                            {
+                                errors = true;
+                            }
+                        }
+                        break;
+
+                    case 2: // jpeg
+                        {
+                            try
+                            {
+                                this.chart.SaveImage(fname, System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Jpeg);
+                            }
+                            catch
+                            {
+                                errors = true;
+                            }
+
+                        }
+                        break;
+                } // switch
+
+                if(errors)
+                {
+                    MessageBox.Show("Ошибка экспорта!", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Картинка экспортирована.", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
     }
 }
