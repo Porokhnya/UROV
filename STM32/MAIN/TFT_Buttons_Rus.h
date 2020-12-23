b7d7934d0d2c901fd70a6464c55bbf55dc4b5f3d
Porokhnya Dmitry <spywarrior@gmail.com>
2020-12-23 11:08:57 +0300

    Update TFT_Buttons_Rus.h

--- STM32/MAIN/TFT_Buttons_Rus.h
+++ STM32/MAIN/TFT_Buttons_Rus.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// класс обработки экранных кнопок
 
 #include <Arduino.h>
 #include "TFTRus.h"
@@ -9,18 +10,18 @@
 
 // флаги состояний кнопок
 
-#define BUTTON_DISABLED			1
-#define BUTTON_SYMBOL			2
-#define BUTTON_HAS_ICON   4
+#define BUTTON_DISABLED			1   // выключена
+#define BUTTON_SYMBOL			2   // символ
+#define BUTTON_HAS_ICON   4   // имеет иконку
 //#define BUTTON_SYMBOL_REP_3X	4
 //#define BUTTON_BITMAP			8	
-#define BUTTON_NO_BORDER		16
-#define BUTTON_HAS_BACK_COLOR 32
-#define BUTTON_HAS_FONT_COLOR 64
-#define BUTTON_VISIBLE 128
-#define BUTTON_SELECTED 256
-#define BUTTON_HAS_FONT 512
-#define BUTTON_UNUSED      0x8000
+#define BUTTON_NO_BORDER		16  // без границы
+#define BUTTON_HAS_BACK_COLOR 32  // имеет фоновый цвет
+#define BUTTON_HAS_FONT_COLOR 64  // имеет цвет шрифта
+#define BUTTON_VISIBLE 128  // видима
+#define BUTTON_SELECTED 256 // выбрана
+#define BUTTON_HAS_FONT 512 // имеет шрифт
+#define BUTTON_UNUSED      0x8000 // не используется
 
 
 #pragma pack(push,1)
@@ -44,34 +45,34 @@ class TFT_Buttons_Rus
 		TFT_Buttons_Rus(TFT_Class *ptrUTFT, TOUCH_Class *ptrURTouch, TFTRus* pTFTRus, int count_btns = MAX_BUTTONS);
     ~TFT_Buttons_Rus();
 
-		int		addButton(uint16_t x, uint16_t y, uint16_t width, uint16_t height, const char *label, uint16_t flags=0);
-		void	drawButtons(DrawButtonsUpdateFunc func=NULL);
-		void	drawButton(int buttonID);
-		void	enableButton(int buttonID, boolean redraw=false);
-		void	disableButton(int buttonID, boolean redraw=false);
-		void	relabelButton(int buttonID, const char *label, boolean redraw=false);
-		boolean	buttonEnabled(int buttonID);
-		void	deleteButton(int buttonID);
-		void	deleteAllButtons();
-		int		checkButtons(OnCheckButtonsFunc pressed = NULL, OnCheckButtonsFunc released = NULL);
-		void	setTextFont(FONTTYPE font);
-		void	setSymbolFont(FONTTYPE font);
-		void	setButtonColors(word atxt, word iatxt, word brd, word brdhi, word back);
-    void selectButton(int buttonID, bool selected, boolean redraw=false);
+		int		addButton(uint16_t x, uint16_t y, uint16_t width, uint16_t height, const char *label, uint16_t flags=0); // добавляет кнопку
+		void	drawButtons(DrawButtonsUpdateFunc func=NULL); // рисует кнопки на экране
+		void	drawButton(int buttonID); // рисует кнопку
+		void	enableButton(int buttonID, boolean redraw=false); // включает кнопку
+		void	disableButton(int buttonID, boolean redraw=false); // выключает кнопку
+		void	relabelButton(int buttonID, const char *label, boolean redraw=false); // меняет текст на кнопке
+		boolean	buttonEnabled(int buttonID); // проверяет, включена ли кнопка
+		void	deleteButton(int buttonID); // удаляет кнопку
+		void	deleteAllButtons(); // удаляет все кнопки
+		int		checkButtons(OnCheckButtonsFunc pressed = NULL, OnCheckButtonsFunc released = NULL); // проверяет кнопки на нажатие
+		void	setTextFont(FONTTYPE font); // устанавливает шрифт текста для всех кнопок
+		void	setSymbolFont(FONTTYPE font); // устанавливает шрифт символов для всех кнопок
+		void	setButtonColors(word atxt, word iatxt, word brd, word brdhi, word back); // устанавливает цвета для всех кнопок
+    void selectButton(int buttonID, bool selected, boolean redraw=false); // выделяет кнопку
     
-		word getButtonBackColor(int buttonID);
-		void 	setButtonBackColor(int buttonID, word color);
+		word getButtonBackColor(int buttonID); // возвращает фоновый цвет кнопки
+		void 	setButtonBackColor(int buttonID, word color); // устанавливает фоновый цвет кнопки
 
-   void setButtonFont(int buttonID, FONTTYPE font);
-		word getButtonFontColor(int buttonID);
-		void 	setButtonFontColor(int buttonID, word color);
+   void setButtonFont(int buttonID, FONTTYPE font); // устанавливает шрифт кнопки
+		word getButtonFontColor(int buttonID); // возвращает цвет шрифта кнопки
+		void 	setButtonFontColor(int buttonID, word color); // устанавливает цвет шрифта кнопки
    
-    void showButton(int buttonID, boolean redraw=false);
-    void hideButton(int buttonID, boolean redraw=false);
-    const char* getLabel(int buttonID);
+    void showButton(int buttonID, boolean redraw=false); // показывает кнопку
+    void hideButton(int buttonID, boolean redraw=false); // скрывает кнопку
+    const char* getLabel(int buttonID); // возвращает текст кнопки
 
-    void setIconFont(FONTTYPE font);
-    void setButtonHasIcon(int buttonID);
+    void setIconFont(FONTTYPE font); // устанавливает шрифт иконки кнопки
+    void setButtonHasIcon(int buttonID); // устанавливает флаг, что первый символ текста кнопки - это иконка
 
 	protected:
 		TFT_Class		*_UTFT;
