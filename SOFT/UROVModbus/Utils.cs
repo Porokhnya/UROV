3054b93885be3a02f837697b570d4bea379c22d7
Porokhnya Dmitry <spywarrior@gmail.com>
2021-04-21 12:38:18 +0300

    approximate

--- SOFT/UROVModbus/Utils.cs
+++ SOFT/UROVModbus/Utils.cs
@@ -374,8 +374,8 @@ namespace UROVModbus
             // копируем данные в список
             ApproximatedInterruptData = InterruptData.ToList();
 
-            // аппроксимируем за 2 прохода
-            doApproximate(2);
+            // аппроксимируем за 1 проход
+            doApproximate(1);
 
             // если вдруг списки не одинаковы - копируем исходный
             if(ApproximatedInterruptData.Count < InterruptData.Count)
